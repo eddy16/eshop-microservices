32de4a28a3ced3dcdfe2ce4309944240ec421263
Edgar Cirilo <edcirilo_9216@live.com.mx>
2018-02-11 23:32:03 -0600

    adding discovery server

--- discovery-service/src/main/java/com/micro/eshop/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/micro/eshop/discoveryservice/DiscoveryServiceApplication.java
@@ -3,25 +3,15 @@ package com.micro.eshop.discoveryservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
-	}
-}
-
-@RestController
-class MessageRestController {
-
-	@Value("${message:default}")
-	private String message;
-
-	@RequestMapping("/message")
-	String getMessage() {
-		return this.message;
 	}
 }
